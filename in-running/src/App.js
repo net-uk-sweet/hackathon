05ade77935a83f5e083b7764662634b550130750
Ian Watson <iwatson@williamhill.co.uk>
2019-07-19 18:08:10 +0100

    tidy up

--- in-running/src/App.js
+++ in-running/src/App.js
@@ -7,25 +7,13 @@ import Bottom from './Bottom';
 import './Timeform.css';
 import './App.css';
 
-export default class App extends React.Component {
-
-  // componentWillMount() {
-  //   const url = 'ws://localhost:1337'
-  //   const connection = new WebSocket(url);
-
-  //   connection.onopen = (msg) => {
-  //     console.log('hello');
-  //   }
-  // }
-
-  render() {
-    return (
-        <BrowserRouter>
-          <Switch>
-            <Route path="/" exact={true} component={Bottom} />
-            <Route path="/top" exact={true} component={Top} />
-          </Switch>
-        </BrowserRouter>
-    );
-  }
+export default function() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact={true} component={Bottom} />
+        <Route path="/top" exact={true} component={Top} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
